7bd4622bce77959f098f47cb1594086569cd71a4
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-06-17 08:43:21 -0300

    Fix test target
    There are two problems with `test.bash` when it
    comes to the expected result: it does not calls
    the local copy of xmenu.bash in systems with the
    extension globally installed, and, in systems
    without it, it does not executes the local copy.
    
    Fix the first problem by creating an unique name
    for the local copy:
        > xmenu_$(date '+%F')_${hash_name:0:13}
    in which `hash_name` is the output of `sha1sum` on
    the `xmenu.bash` file.
    
    Fix the second one by copying the file with
    `install -m 0700` instead of `cp`.

--- test_env/test.bash
+++ test_env/test.bash
@@ -3,6 +3,9 @@
 set -e
 
 unset PASSWORD_STORE_SIGNING_KEY
+unset PASSWORD_STORE_DIR
+unset PASSWORD_STORE_EXTENSIONS_DIR
+unset PASSWORD_STORE_ENABLE_EXTENSIONS
 TEST_ENV_DIR="${PWD}/test_env"
 export PASSWORD_STORE_DIR="${TEST_ENV_DIR}/password-store/db"
 export PASSWORD_STORE_EXTENSIONS_DIR="${TEST_ENV_DIR}/password-store/extensions"
@@ -11,7 +14,6 @@ export GNUPGHOME="${TEST_ENV_DIR}/gnupg"
 PGP_DIR="${TEST_ENV_DIR}/pgp"
 PGP_PRIV_KEY="${PGP_DIR}/masterkey.asc"
 PGP_KEY_ID="2C752423906F4CE73091C2077B18C1616AAA9C35"
-
 #
 # Create the necessary directories.
 #
@@ -36,5 +38,10 @@ pass generate -f onlyPassword
 #
 # Copy Extension.
 #
-cp xmenu.bash "${PASSWORD_STORE_EXTENSIONS_DIR}"
-pass xmenu "${@}"
+# Were the extension to be copied with the same name, this script would ignore
+# it in every system that had it globally installed.
+#
+hash_name="$(sha1sum xmenu.bash)"
+hash_name="xmenu_$(date '+%F')_${hash_name:0:13}.bash"
+install -m 0700 xmenu.bash "${PASSWORD_STORE_EXTENSIONS_DIR}/${hash_name}"
+pass "${hash_name%.bash}" "${@}"
