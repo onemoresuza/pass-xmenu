47f9422a68256e1e5abcdd3bdb836298721e6b20
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-06-20 16:10:45 -0300

    Update test script
    Now the test script runs the macro by itself using
    `xdotool`, automating the testing, at least, of
    macros with actions that can be used on the
    terminal: `type` and `paste-term`.

--- test_env/test.bash
+++ test_env/test.bash
@@ -14,6 +14,40 @@ export GNUPGHOME="${TEST_ENV_DIR}/gnupg"
 PGP_DIR="${TEST_ENV_DIR}/pgp"
 PGP_PRIV_KEY="${PGP_DIR}/masterkey.asc"
 PGP_KEY_ID="2C752423906F4CE73091C2077B18C1616AAA9C35"
+MACROFILE="macroFile"
+PASSWORD="pass1234"
+USERNAME="username"
+
+check_macro() {
+  #
+  # Run a macro with the action in ${1}.
+  #
+
+  pass "${HASH_NAME%.bash}" -atrue -A"${1}" &
+  printf "Username: "
+  {
+    sleep 0.1
+    xdotool type --clearmodifiers "${MACROFILE}"
+    xdotool key --clearmodifiers Return
+  } &
+  read -r username
+  printf "Password: "
+  stty -echo
+  read -r password
+  stty echo
+  printf "\n"
+
+  if [[ "${username}" == "${USERNAME}" && "${password}" == "${PASSWORD}" ]]; then
+    printf "%s Macro test successful\n" "${1}"
+  else
+    printf "%s Macro test FAILED!\n" "${1}"
+    printf "Expected Values: Username: %s and Password: %s\n" \
+      "${USERNAME}" "${PASSWORD}"
+    printf "Actual Values: Username: %s and Password: %s\n" \
+      "${username}" "${password}"
+    exit 1
+  fi
+}
 #
 # Create the necessary directories.
 #
@@ -31,17 +65,22 @@ gpg --yes --batch --pinentry loopback --import "${PGP_PRIV_KEY}"
 printf "5\ny\n" \
   | gpg --command-fd 0 --expert --edit-key "${PGP_KEY_ID}" trust
 pass init "${PGP_KEY_ID}"
-printf "password\nUsername: user\nTestCommand: echo\n%s" \
-  "_MACRO: @TestCommand space @Username space @PASSWORD Return" \
-  | pass insert -m macroFile
-pass generate -f onlyPassword
+printf "%s\nUsername: %s\n%s" \
+  "${PASSWORD}" "${USERNAME}" "_MACRO: @Username Return @PASSWORD Return" \
+  | pass insert -m "${MACROFILE}"
 #
 # Copy Extension.
 #
 # Were the extension to be copied with the same name, this script would ignore
 # it in every system that had it globally installed.
 #
-hash_name="$(sha1sum xmenu.bash)"
-hash_name="xmenu_$(date '+%F')_${hash_name:0:13}.bash"
-install -m 0700 xmenu.bash "${PASSWORD_STORE_EXTENSIONS_DIR}/${hash_name}"
-pass "${hash_name%.bash}" "${@}"
+HASH_NAME="$(sha1sum xmenu.bash)"
+HASH_NAME="xmenu_$(date '+%F')_${HASH_NAME:0:13}.bash"
+install -m 0700 xmenu.bash "${PASSWORD_STORE_EXTENSIONS_DIR}/${HASH_NAME}"
+
+#
+# Check the generated key strokes
+#
+clear
+check_macro "type"
+check_macro "paste-term"
